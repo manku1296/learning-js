b4fb628d241c7ce430837b5140323fbacf52ca76
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-04 12:26:36 +0000

    Data Type Learning

---
+++ Basic_JS/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all your JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3
+    +
+    3) // Code readability should be high
+
+console.log("Mayank")
+
+let name = "Mayank"
+let age = 26
+let isLoggedIn = false
+let state; //Undefined
+
+// number => 2 to power 53
+// bigint (Used for Big numbers like Trading websites, Stockmarket)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null) // object
