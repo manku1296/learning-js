ca18701aebb440e4dcd134c04453654d1064a125
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-07 08:43:43 +0000

    Learn Datatypes In Detailed

--- Basic_JS/02_datatype.js
+++ Basic_JS/02_datatype.js
@@ -1,28 +1,29 @@
 "use strict"; // treat all your JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+//alert( 3 + 3) // we are using nodejs, not browser
 
-console.log( 3
-    +
-    3) // Code readability should be high
+//console.log( 3
+//   +
+//    3) // Code readability should be high
 
-console.log("Mayank")
+//console.log("Mayank")
 
-let name = "Mayank"
-let age = 26
-let isLoggedIn = false
-let state; //Undefined
+// let name = "Mayank"
+// let age = 26
 
-// number => 2 to power 53
-// bigint (Used for Big numbers like Trading websites, Stockmarket)
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+//let isLoggedIn = false
+// let state; //Undefined
+// console.log(state)
+// // number => 2 to power 53
+// // bigint (Used for Big numbers like Trading websites, Stockmarket)
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
 
 
-//object
+// //object
 
-console.log(typeof undefined); // Undefined
-console.log(typeof null) // object
+// console.log(typeof undefined); // Undefined
+// console.log(typeof null) // object
