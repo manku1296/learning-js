7a427a18e3c65cd1e5324ddade50115373ff655e
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-08 07:30:41 +0000

    Learning Datatypes and Datatype Conversion Operations

--- Basic_JS/03_datatypeOperations.js
+++ Basic_JS/03_datatypeOperations.js
@@ -1,32 +1,80 @@
-let score = "33abc"
+let score = "Mayank"
 
+// console.log(score);
 // console.log(typeof score);
-// console.log(typeof (score));
 
 let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
-// console.log(typeof valueInNumber)
 
+// 33 => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+let value = 33
+// console.log(value)
+// console.log(typeof value);
 
-let isLoggedIn = "mayank"
+// let valueToString = String(value)
+// console.log(valueToString);
+// console.log(typeof valueToString);
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let info = "33abc"
+// console.log(info);
+// console.log(typeof info);
+
+// let stringToNumber = Number(info)
+// console.log(stringToNumber);
+// console.log(typeof stringToNumber);
+
+let isLoggedIn = ""
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "mayank" => true
 
-let someNumber = 33
-
-let stringNumber = String(someNumber)
-
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
 
 
-// *********************** Operations ***********************
+// ******************* Operations ************************
+
+let digit = 3
+// let finalDigit = -digit
+// console.log(finalDigit);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/2)
+//console.log(2%2)
+
+// console.log( "2" + 3) => 23
+//console.log( "2" + 9 + 8) => 298
+// console.log( 2 + "1") => 21
+// console.log( 2 + 3 + "2" + "4") => 524
+
+//console.log( (3 + 4) * 5 % 3) => 2
+
+// console.log(true) => true
+// console.log(+true) => 1
+// console.log(+"") => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2
+
+// console.table([num1, num2, num3]);
+
+let gameCounter = 50
+++gameCounter;
+// console.log(gameCounter); => 51 ; Read about prefix (++) and postfix (++) 
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
