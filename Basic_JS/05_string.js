e0c06a50fc41ef284ab02755f7f22e2e359a3d7a
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-08 18:06:07 +0000

    String Method

---
+++ Basic_JS/05_string.js
@@ -0,0 +1,15 @@
+const name = "The quick brown fox jumps over the lazy dog."
+
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} my repo count is ${repoCount}`);
+
+const gameName = new String('mayank-hc-com')
+
+//let index = -4
+
+// console.log(`Hello this example belongs to at prototype method ${name.at(index)}`);
+
+// console.log(name.length); //
