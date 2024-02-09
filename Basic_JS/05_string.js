bcc26db918e3f5f0ff3207f2d8a7cb0025f5bb13
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-09 09:15:39 +0000

    String Methods

--- Basic_JS/05_string.js
+++ Basic_JS/05_string.js
@@ -13,3 +13,43 @@ const gameName = new String('mayank-hc-com')
 // console.log(`Hello this example belongs to at prototype method ${name.at(index)}`);
 
 // console.log(name.length); //
+
+//console.log(gameName.blink()); //<blink>mayank-hc-com</blink>
+//console.log(gameName.big()); //<big>mayank-hc-com</big>
+
+//console.log(gameName.bold()); // <b>mayank-hc-com</b>
+
+//console.log(gameName.charAt(5)); //Return 'K' 
+
+// console.log(gameName.charCodeAt(0)); //Return 109
+
+// const icon ='☃★♲'
+// console.log(icon.codePointAt(1)); // Return 9733
+
+// const str1 = 'Hello'
+// const str2 = 'World!'
+// console.log(str1.concat(' ',str2)); //Output: Hello World!
+// console.log(str2.concat(', ',str1)); // Output: World!, Hello
+
+// const str1 = 'Bikes are best!'
+// const str2 = 'My name is Mayank.'
+// console.log(str1.endsWith('best!')); // Output: True
+// console.log(str2.endsWith('Mayank')); // Output: False
+
+// console.log(gameName.fixed()); // <tt>mayank-hc-com</tt>
+
+// console.log(gameName.fontcolor("red")); //<font color="red">mayank-hc-com</font>
+
+// console.log(gameName.fontsize(12)); //<font size="12">mayank-hc-com</font>
+
+// console.log(String.fromCharCode(189, 43, 190, 61)); //½+¾=
+
+// console.log(String.fromCodePoint(9731, 9733, 9842, 0x2f804)); //☃★♲你
+
+// let word = "mayank"
+// console.log(`The word "${word}" ${
+//     gameName.includes(word) ? 'is' : 'is not'
+//   } in the sentence`,); //The word "mayank" is in the sentence
+
+//console.log(gameName.indexOf('k')); // Output is 5
+
