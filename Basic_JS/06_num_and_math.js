d0f6324f94d22f0b370e9285b204513e0c0cb3a8
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-11 09:45:58 +0000

    Num_and_math

---
+++ Basic_JS/06_num_and_math.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));// Output is 100.00 (This methon, Basically use for E-Commerce)
+
+const otherValue = 123.399
+
+// console.log(otherValue.toPrecision(4)) //Output will be 123.9 It's give you precise value.
+
+const rupees = 1000000
+
+// console.log(rupees.toLocaleString('en-IN')); //Output will be 10,00,000 in this format
+
+
+//++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);// Output => object[Math]{}
+// console.log(Math.abs(-4)); // Output => 4
+// console.log(Math.round(4.6)); // Output will be round(4.6) => 5; round(4.3) => 4
+// console.log(Math.ceil(4.9)); // Output will be ceil(4.1/2/3/4/5/6/7/8/9) => 5
+// console.log(Math.floor(5.855)); // output will be floor(5.1/2/3/4/5/6/7/8/9) => 5
+// console.log(Math.min(3, 5, 2, 9)); // Output will be min(3,5,2,9) => 2
+// console.log(Math.max(5,17,8,9)); // Output will be max(5,17,8,9) => 17
