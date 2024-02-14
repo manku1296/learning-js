55314e50e9d26db07595a352557f1a37675abaa2
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-14 07:30:54 +0000

    Functions Learning

---
+++ 03_Basic_JS/01_function.js
@@ -0,0 +1,25 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+    console.log("K");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log( number1 + number2)
+// }
+
+// addTwoNumbers(5, 4) // Output => 9
+
+function addTwoNumbers(number1, number2){
+
+    return number1 + number2
+
+}
+
+const result = addTwoNumbers(2, 5)
+console.log(result);// Output => 7
