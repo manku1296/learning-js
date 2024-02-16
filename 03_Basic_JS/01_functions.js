deabff74b7909a585871e5e1b01582fa04da0bd2
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-16 16:59:12 +0000

    Using Array and Object in Functions

--- 03_Basic_JS/01_functions.js
+++ 03_Basic_JS/01_functions.js
@@ -43,4 +43,44 @@ function loginUserMessage(username = "mayank"){
 }
 
 // console.log(loginUserMessage("Mayank"));// Method-1
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+//+++++++++++++++++++++++ Rest Operator +++++++++++++++++++++++
+
+function calculateCartPrice(val1, val2 ,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 500, 700, 1200)) //(...num1) Rest Operator Output => [ 200, 500, 700 ], (val1, val2,...num1) => [ 700, 1200 ]
+
+
+
+/*Using Objects in Function*/
+const user = {
+    username: "Manku",
+    price: 4500
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user) // Method-1
+// handleObject({  //Method-2
+//     username: "Mayank",
+//     price: 4000
+// })
+
+
+
+/*Using Arrays in Functions*/
+
+const newArray = [200, 400, 600, 900]
+
+function returnSecondValue(getArray){
+    return(getArray[1])
+}
+
+// console.log(returnSecondValue(newArray))// Method-1 For printing output
+console.log(returnSecondValue([200, 300, 500])); //Method-2 for printing output
