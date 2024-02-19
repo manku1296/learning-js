5a7297ffcbe13d5f2217acd58f86649fad666765
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-19 13:11:49 +0000

    Learning Switch Control Statement

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,44 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "september"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "august":
+        console.log("august");
+        break;
+    case "september":
+        console.log("september");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
