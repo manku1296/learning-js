f6a291b14cba3c9183631c3838061dafd7cbfb5f
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-20 11:18:50 +0000

    While and Do While loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//while
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of Index is ${index}`);
+//     index = index + 2
+// }
+
+let friendsArray = ["Bhanu", "Rohit", "Pavan", "Yash"]
+let arr = 0
+while (arr <= friendsArray.length) {
+    // console.log(`Value is ${friendsArray}`);
+    arr = arr + 1
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
