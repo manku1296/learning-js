d22cdfe4190c860c41d94f5e3e70633ea31b0ad7
manku1296 <100944739+manku1296@users.noreply.github.com>
2024-02-20 18:11:27 +0000

    For Of loop, Maps and Arrays

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -11,5 +11,30 @@ for (const num of arr) {
 
 const greetings = "Hello Mayank!"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
 }
+
+
+//Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('Aus', "Australia")
+map.set('Fr', "France")
+map.set('Nep', "Nepal")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':- ',value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
